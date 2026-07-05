ff96762c8a07cc797c166d26a6ed6ab94acbfe95
manthan8008 <jainn.manthann@gmail.com>
2026-07-05 19:39:55 +0530

    commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:5000",
+      "/api": "https://financeflow-backend-58w4.onrender.com",
     },
   },
   build: {
